ac26aa3c396423569e455117a432a1333a51bb85
nikhilpn725 <nikhilpn725@gmail.com>
2023-12-09 01:45:10 +0530

    bookswap logo added

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,42 @@
+import Link from "next/link";
+import Wrapper from "./Wrapper";
+
 const Header = () => {
   return (
     <>
       <header>Hello</header>
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link
+          href="/"
+          // onClick={() => setMobileMenu(false)}
+          className="hidden md:block transition-transform active:scale-95"
+        >
+          <img
+            src="/assets/bookswaplogo.svg"
+            alt="main logo"
+            className="w-[100px] md:w-[140px]"
+          />
+        </Link>
+        {/* <div>
+            <button
+              onClick={() => {
+                console.log("logout");
+                signOut();
+              }}
+            >
+              LogOut
+            </button>
+          </div> */}
+        {/* <Search></Search> */}
+
+        {/* <NavIcons
+          authUser={authUser}
+          showProfileMenu={showProfileMenu}
+          setShowProfileMenu={setShowProfileMenu}
+          signOut={signOut}
+          setLogOut={setLogOut}
+        ></NavIcons> */}
+      </Wrapper>
     </>
   );
 };
